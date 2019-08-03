9f9606c429e347cc5c77347a4a8ce019abfd39c6
SunilMusinada <Sunil.chaitanya@gmail.com>
2019-08-03 23:23:39 +0530

    Added sign in capability with email and password

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./sign-in.styles.scss";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
-import { signInwithGoogle } from "../../firebase/firebase.utils";
+import { signInwithGoogle, auth } from "../../firebase/firebase.utils";
 
 class SignIn extends Component {
   constructor(props) {
@@ -12,10 +12,16 @@ class SignIn extends Component {
       password: ""
     };
   }
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
 
-    this.setState({ email: "", password: "" });
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.log("Error occured to authenticate the user", error.message);
+    }
   };
 
   handleChange = event => {
