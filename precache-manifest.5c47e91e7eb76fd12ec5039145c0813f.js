be9597d5525d0ac597d24e8056aeed2aca03343f
SunilMusinada <Sunil.chaitanya@gmail.com>
2019-08-04 21:12:32 +0530

    Updates

---
+++ precache-manifest.5c47e91e7eb76fd12ec5039145c0813f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73ddd1f5525df46a30c80eadfc822699",
+    "url": "/ind-clothing/index.html"
+  },
+  {
+    "revision": "adb9ac925081c767e204",
+    "url": "/ind-clothing/static/css/main.1ceda8ac.chunk.css"
+  },
+  {
+    "revision": "02250aeb80203e72bf55",
+    "url": "/ind-clothing/static/js/2.e3737ea9.chunk.js"
+  },
+  {
+    "revision": "adb9ac925081c767e204",
+    "url": "/ind-clothing/static/js/main.b27bebb3.chunk.js"
+  },
+  {
+    "revision": "0caa5aa8392c7b4647b6",
+    "url": "/ind-clothing/static/js/runtime~main.c22cefd0.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/ind-clothing/static/media/crown.ed28accb.svg"
+  }
+]);
